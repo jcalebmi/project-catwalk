4c62a4219142ce414c156c153ffed966b108b295
Amber Morris <ambermorris1997@gmail.comgit config --global core.editor vimgit config --global core.autocrlf inputgit config --global user.email ambermorris1997@gmail.com>
2021-04-16 17:27:19 -0400

    Implemented Redux store!

--- client/src/Components/app.jsx
+++ client/src/Components/app.jsx
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 /* The provider component makes the Redux store available to any nested
     components that need to access the store */
 import { Provider } from 'react-redux';
 import Count from './Count.jsx';
 import store from '../store/store.jsx';
+import Overview from '../components/overview/Overview.jsx'
 
-const App = () => (
-  <Provider store={store}>
-    <div>
-      <h1>Hello World </h1>
-      <Count />
-    </div>
-  </Provider>
-);
+const axios = require('axios');
+
+class App extends Component {
+
+  componentDidMount() {
+
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products', {headers: {authorization: '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'}})
+    .then((res) => {
+      console.log(res.data)
+      store.dispatch({
+          type: 'UPDATE_CURRENT_PRODUCT',
+          payload: res.data[0]
+        })
+        store.dispatch({
+          type: 'UPDATE_ALL_PRODUCTS',
+          payload: res.data
+        })
+      })
+      .catch((err) => {
+        console.log('ERROR: ', err);
+      });
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <div>Catwalk</div>
+        <Overview />
+      </Provider>
+    );
+  }
+}
+// const App = () => (
+//   <Provider store={store}>
+//     <div>
+//       <h1>Hello World </h1>
+//       <Count />
+//     </div>
+//   </Provider>
+// );
 
 // function App() {
 //   const [count, setCount] = useState(0);
@@ -23,4 +56,5 @@ const App = () => (
 //     </h1>
 //   );
 // }
+
 export default App;
---
+++ client/src/reducers/productFetcher.jsx
@@ -0,0 +1,15 @@
+const productFetcher = (state = [], action) => {
+  switch (action.type) {
+    case 'UPDATE_CURRENT_PRODUCT':
+      return {
+        product: action.payload,
+      }
+    case 'UPDATE_ALL_PRODUCTS':
+      return {
+        products: action.payload,
+      }
+    default:
+      return state;
+  }
+}
+export default productFetcher;
