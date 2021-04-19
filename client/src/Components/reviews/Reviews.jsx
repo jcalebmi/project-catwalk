28a20e82f519d46400f79f822b719173b55e3e83
Caleb Iuliano <calebiuliano@gmail.com>
2021-04-19 18:51:51 -0400

    Cleaned up eslinter Code

--- client/src/Components/reviews/Reviews.jsx
+++ client/src/Components/reviews/Reviews.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import ReviewItem from './ReviewItem.jsx';
 import getReviews from './helpers/getReviews.js';
 import AddReview from './AddReview.jsx';
