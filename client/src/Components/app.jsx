833a1ecbe81319cb77dd896f260864b02e21b439
Max Tickman <maxiant1994@gmail.com>
2021-04-19 14:52:49 -0500

    Refactored app.jsx to functional, refactored app.jsx to call api from the server, fixed linter

--- client/src/Components/app.jsx
+++ client/src/Components/app.jsx
@@ -1,70 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 /* The provider component makes the Redux store available to any nested
     components that need to access the store */
 import { Provider } from 'react-redux';
-import Count from './Count.jsx';
 import store from '../store/store.jsx';
 import Overview from '../Components/overview/Overview.jsx'
+
 //Review Imports
 import Reviews from './reviews/Reviews.jsx';
 import sampleData from '../../dist/sampleData.js';
 import Ratings from './reviews/Ratings.jsx';
-import apiToken from '../../../myconfig.js';
 
-const axios = require('axios');
+//Initialization of Products
+import setProducts from './helpers/setProducts.jsx';
 
-class App extends Component {
+function App() {
+  useEffect(() => {
+    setProducts();
+  })
 
-  componentDidMount() {
-
-    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products', {headers: {authorization: apiToken}})
-    .then((res) => {
-      //console.log(res.data)
-      store.dispatch({
-          type: 'UPDATE_CURRENT_PRODUCT',
-          payload: res.data[0]
-        })
-        store.dispatch({
-          type: 'UPDATE_ALL_PRODUCTS',
-          payload: res.data
-        })
-      })
-      .catch((err) => {
-        console.log('ERROR: ', err);
-      });
-  }
-
-
-  render() {
-    return (
-      <Provider store={store}>
-        <div>Catwalk</div>
-        <Overview />
-        <div id="ratingReviewContainer">
-          <Ratings />
-          <Reviews />
-        </div>
-      </Provider>
-    );
-  }
+  return (
+    <Provider store={store}>
+      <div>Catwalk</div>
+      <Overview />
+      <div id="ratingReviewContainer">
+        <Ratings />
+        <Reviews />
+      </div>
+    </Provider>
+  );
 }
-// const App = () => (
-//   <Provider store={store}>
-//     <div>
-//       <h1>Hello World </h1>
-//       <Count />
-//     </div>
-//   </Provider>
-// );
-
-// function App() {
-//   const [count, setCount] = useState(0);
-//   return (
-//     <h1 onClick={() => { setCount(count + 1); }}>
-//       Hello World #
-//       {count}
-//     </h1>
-//   );
-// }
 
 export default App;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const path = require('path');
+const getProducts = require('./appHelpers/getProducts.js');
 const getReviews = require('./reviewHelpers/getReviews.js');
 const updateHelpful = require('./reviewHelpers/helpfulness.js');
 const getMetaData = require('./reviewHelpers/getMeta.js')
@@ -13,10 +14,17 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
-app.get('/test', (req, res) => {
-  res.status = 200;
-  res.send('passed!');
-});
+
+//Takes product ID & calls Axios helper
+//in appHelpers/getProducts.js
+//Returns products back to client
+app.get('/products', (req, res) => {
+  var id = req.params.id;
+  getProducts(id).then(response => {
+    res.status(200);
+    res.send(response);
+  });
+})
 
 //Takes product ID & calls Axios helper
 //in reviewHelpers/getReviews.js
