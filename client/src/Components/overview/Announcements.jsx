2378e3767f1f79e776cc845336cec980ea0e7f60
Max Tickman <maxiant1994@gmail.com>
2021-04-30 14:34:01 -0500

    last minute changes

--- client/src/Components/overview/Announcements.jsx
+++ client/src/Components/overview/Announcements.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-
-const selectProductById = (state) => state.product;
-const selectAllProducts = (state) => state.products;
+import setProduct from '../helpers/setProduct.jsx';
 
 const Announcements = () => (
   <div className='announcement'>
     SITE-WIDE ANNOUNCEMENT MESSAGE! -- SALE/DISCOUNT
     <b> OFFER </b>
-    <a className='underline'>NEW PRODUCT HIGHLIGHT</a>
+    <span onClick={() => {
+      setProduct(19098);
+    }} className='underline productHighlight'>NEW PRODUCT HIGHLIGHT</span>
   </div>
 );
 export default Announcements;
